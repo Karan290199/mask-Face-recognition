dc6b8c8a90ef83555b9c44a7b2cbf140bef9504c
Redakai290199 <>
2020-09-10 16:00:40 +0530

    mask+face

---
+++ mask_detection.py
@@ -0,0 +1,74 @@
+import numpy as np
+import cv2
+import random
+import os
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('./trainer/trainer.yml')
+face_cascade = cv2.CascadeClassifier('./Cascades/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('./Cascades/haarcascade_eye.xml')
+mouth_cascade = cv2.CascadeClassifier('./Cascades/haarcascade_mcs_mouth.xml')
+upper_body = cv2.CascadeClassifier('./Cascades/haarcascade_upperbody.xml')
+bw_threshold = 80
+font = cv2.FONT_HERSHEY_SIMPLEX
+org = (30, 30)
+weared_mask_font_color = (255, 255, 255)
+not_weared_mask_font_color = (0, 0, 255)
+thickness = 2
+font_scale = 1
+weared_mask = "Thank You for wearing MASK"
+not_weared_mask = "Please wear MASK to defeat Corona"
+id = 0
+names = ['None', 'Karan'] #Put your respective names 
+cap = cv2.VideoCapture(0)
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    (thresh, black_and_white) = cv2.threshold(gray, bw_threshold, 255, cv2.THRESH_BINARY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    faces_bw = face_cascade.detectMultiScale(black_and_white, 1.1, 4)
+    if(len(faces) == 0 and len(faces_bw) == 0):
+        cv2.putText(img, "No face found...", org, font, font_scale, weared_mask_font_color, thickness, cv2.LINE_AA)
+    elif(len(faces) == 0 and len(faces_bw) == 1):
+        cv2.putText(img, weared_mask, org, font, font_scale, weared_mask_font_color, thickness, cv2.LINE_AA)
+    else:
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            roi_color = img[y:y + h, x:x + w]
+            mouth_rects = mouth_cascade.detectMultiScale(gray, 1.5, 5)
+        if(len(mouth_rects) == 0):
+            cv2.putText(img, weared_mask, org, font, font_scale, weared_mask_font_color, thickness, cv2.LINE_AA)
+            cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+            id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+            if (confidence < 100):
+                id = names[id]
+                confidence = "  {0}%".format(round(confidence))
+            else:
+                id = "unknown"
+                confidence = "  {0}%".format(round(100 - confidence))        
+            cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+            cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+        else:
+            for (mx, my, mw, mh) in mouth_rects:
+                if(y < my < y + h):
+                    cv2.putText(img, not_weared_mask, org, font, font_scale, not_weared_mask_font_color, thickness, cv2.LINE_AA)
+                    cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+                    id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+                    if (confidence < 100):
+                        id = names[id]
+                        confidence = "  {0}%".format(round(confidence))
+                    else:
+                        id = "unknown"
+                        confidence = "  {0}%".format(round(100 - confidence))        
+                    cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+                    cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+                    break
+    cv2.imshow('Mask Detection', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+# Release video
+cap.release()
+cv2.destroyAllWindows()
