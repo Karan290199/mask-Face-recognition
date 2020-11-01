906ec751944c221aa6fd940206b29793b668c6c8
Redakai290199 <mailtokks290199@gmail.com>
2020-11-01 10:33:28 +0530

    Update face_dataset.py

--- face_dataset.py
+++ face_dataset.py
@@ -1,10 +1,10 @@
 import cv2
-import os
 cam = cv2.VideoCapture(0)
 cam.set(3, 640) # set video width
-cam.set(4, 480) # set video height
+cam.set(4, 640) # set video height
 face_detector = cv2.CascadeClassifier('./Cascades/haarcascade_frontalface_default.xml')
 face_id = input('\n enter user id end press <return> ==>  ')
+face_name = input('\n enter your name and press <return> ==>  ')
 print("\n [INFO] Initializing face capture. Look the camera and wait ...")
 count = 0
 while(True):
@@ -14,12 +14,12 @@ while(True):
     for (x,y,w,h) in faces:
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
         count += 1
-        cv2.imwrite("./dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+        cv2.imwrite("./dataset/" +str(face_name)+'.'+ str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
         cv2.imshow('image', img)
     k = cv2.waitKey(100) & 0xff 
     if k == 27:
         break
-    elif count >= 30:
+    elif count >= 60:
          break
 print("\n [INFO] Exiting Program and cleanup stuff")
 cam.release()
